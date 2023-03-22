e19e4a477c76f0906508170b6f767bff14f09443
Krisztina Nyitrai <krisztany@gmail.com>
2023-03-22 13:46:00 +0000

    News API added. Cards generated with inspiring news & made clickable.

--- app.js
+++ app.js
@@ -70,3 +70,65 @@
 
         pointsTotal.textContent = newPoints;
     };
+
+
+// News API
+
+    const newsImage = document.getElementById("news-image");
+    const newsTitle = document.getElementById("news-title");
+    const newsSourceDate = document.getElementById("source-and-date");
+
+    const url = 'https://newsapi.org/v2/everything?' +
+          'q=(uplifting AND (charity OR inspirational OR sustainability OR ethical OR "positive news" OR uplifting OR nonprofit OR volunteerring OR fundraising OR "humanitarian aid" OR "community development"))&' +
+          'sortBy=relevancy&' +
+          'apiKey=6400460033fb46179b5edf2c0cbaecad';
+
+    const req = new Request(url);
+
+    fetch(req)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+        UpdateDisplay(data);
+        })
+    .catch(error => console.log(error));
+
+    const newsContainer = document.getElementById("news-container");
+
+    function UpdateDisplay(data) {
+        let ArticlesArray = data.articles;
+        
+        // Initialize an empty string to hold the HTML for the ion-cards
+        let cardsHtml = "";
+
+        // Loop through the first 10 articles in the array
+        for (let i = 0; i < 10; i++) {
+            let article = ArticlesArray[i];
+            
+            // Create the HTML for the ion-card using template literals
+            let cardHtml = `
+            <a href="${article.url}">
+            <ion-card>
+                <ion-item lines="none">
+                <ion-thumbnail slot="start">
+                    <img id="news-image" alt="News Article Image" src="${article.urlToImage || 'https://ionicframework.com/docs/img/demos/thumbnail.svg'}" />
+                </ion-thumbnail>
+                <ion-label>
+                    <h2 id="news-title" class="ion-text-wrap">${article.title}</h2>
+                    <p id="source-and-date">${article.source.Name} | ${new Date(article.publishedAt).toLocaleDateString()}</p>
+                </ion-label>
+                <ion-icon name="heart-outline"></ion-icon>
+                </ion-item>
+            </ion-card>
+            </a>
+            `;
+            
+            // Append the HTML for the ion-card to the cardsHtml string
+            cardsHtml += cardHtml;
+        }
+        
+        // Set the innerHTML of the newsContainer element to the cardsHtml string
+        newsContainer.innerHTML = cardsHtml;
+    }
+
+// NEXT UP: fix grass, add points, make articles savable
